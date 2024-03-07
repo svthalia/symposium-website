1abb3a76fa877ad415fafd9d22a570ec9cf05904
Jeroen Brinkhorst <jeroenb04@live.com>
2024-03-07 12:49:20 +0100

    Markup :)

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ const pageNames = [
     "about.html",
 ];
 
-const fileName = location.href.split("/").slice(-1); 
+const fileName = location.href.split("/").slice(-1);
 
 const activeLoadBar = "[---------------------------------]";
 const inactiveLoadBar = "Requesting Access."
@@ -32,7 +32,7 @@ const inactiveLoadBar = "Requesting Access."
 /********************/
 
 //  Template for replacing characters within a string.
-String.prototype.replaceAt = function(indexToReplace, replacement) {
+String.prototype.replaceAt = function (indexToReplace, replacement) {
     return this.substring(0, indexToReplace) + replacement + this.substring(indexToReplace + replacement.length);
 }
 
@@ -40,27 +40,27 @@ String.prototype.replaceAt = function(indexToReplace, replacement) {
 //  the shell highlighter. *.start() starts the global timer,
 //  and *.stop() stops the global timer.
 function blinkerTimer() {
-//  The two possible states.
+    //  The two possible states.
     var stop, resume;
 
-//  Starting the timer by creating a new instance.
+    //  Starting the timer by creating a new instance.
     this.start = () => {
         timer = setInterval(() => {
-//  Updating the command highlighter
+            //  Updating the command highlighter
             let commandHighlighter = document.getElementById("highlighter");
 
-//  Fliping the states of the highlighter.
-            if(commandHighlighter.style.visibility == "hidden")
+            //  Fliping the states of the highlighter.
+            if (commandHighlighter.style.visibility == "hidden")
                 commandHighlighter.style.visibility = "visible";
-    
+
             else
 
-            commandHighlighter.style.visibility = "hidden";
+                commandHighlighter.style.visibility = "hidden";
         }, 500)
     };
 
-//  Stopping the timer by killing its current instance,
-//  and making sure the blinker is visible.
+    //  Stopping the timer by killing its current instance,
+    //  and making sure the blinker is visible.
     this.stop = () => {
         clearInterval(timer);
         document.getElementById("highlighter").style.visibility = "visible";
@@ -83,30 +83,30 @@ function sleep(ms) {
 //  html element, and 'speed' denotes how fast the 
 //  text is reconstructed.
 async function reconstructInnerHtmlID(htmlElementID, speed) {
-//  Storing the specified html element and its text contents.
+    //  Storing the specified html element and its text contents.
     let htmlEL = document.getElementById(htmlElementID);
     let initialString = htmlEL.innerText;
 
-//  Setting the visibility on, and deleting the
-//  original text contents of the html element,
-//  in order to make the efect pop out.
+    //  Setting the visibility on, and deleting the
+    //  original text contents of the html element,
+    //  in order to make the efect pop out.
     htmlEL.style.visibility = "visible";
     htmlEL.innerText = "";
 
-//  Going through each element of the intial string
-//  and adding it sequentiantly to the element's
-//  contents again.
-    for(i = 0; i < initialString.length; i++) {
+    //  Going through each element of the intial string
+    //  and adding it sequentiantly to the element's
+    //  contents again.
+    for (i = 0; i < initialString.length; i++) {
         htmlEL.innerText = htmlEL.innerText + initialString.charAt(i);
 
-//  Spaces work a bit different, so enforcing it for now.
-//  Also, a little more waiting time to add some 'pizzaz'.
-        if(initialString.charAt(i) == ' ') {
+        //  Spaces work a bit different, so enforcing it for now.
+        //  Also, a little more waiting time to add some 'pizzaz'.
+        if (initialString.charAt(i) == ' ') {
             await sleep(speed + 50);
-            htmlEL.innerText = htmlEL.innerText +'\xa0';
+            htmlEL.innerText = htmlEL.innerText + '\xa0';
         }
-            
-//  Delay after each letter.
+
+        //  Delay after each letter.
         await sleep(speed);
     }
 }
@@ -115,7 +115,7 @@ async function reconstructInnerHtmlID(htmlElementID, speed) {
 //  command is denoted by the commandIdent which corresponds
 //  to an existing command.
 async function writeCurrentCommandToShell(commandIdent) {
-//  Briefly chaging the contents of the highlighter to achieve the animation.
+    //  Briefly chaging the contents of the highlighter to achieve the animation.
     document.getElementById("highlighter").innerText = "";
     document.getElementById("highlighter").innerText = rootCat + pageNames[commandIdent];
     await reconstructInnerHtmlID("highlighter", 50);
@@ -130,22 +130,22 @@ async function writeCurrentCommandToShell(commandIdent) {
 //  presses at a time. 'buttonIdent' just identifies
 //  which button was pressed.
 async function buttonPress(buttonIdent) {
-    if(!isButtonPressed) {
-//  Breifly stopping the highlighting animation.
+    if (!isButtonPressed) {
+        //  Breifly stopping the highlighting animation.
 
         blink.stop();
         await resetOnInnerPageLoad();
-        
 
-//  Flaging the button as pressed and writing to the shell.
+
+        //  Flaging the button as pressed and writing to the shell.
         isButtonPressed = true;
         await writeCurrentCommandToShell(buttonIdent);
 
-//  Incrementing the button press counter in order 
-//  correctly display the commands.
+        //  Incrementing the button press counter in order 
+        //  correctly display the commands.
         buttonPresses++;
 
-//  Flagging the button as released.
+        //  Flagging the button as released.
         isButtonPressed = false;
     }
 }
@@ -171,52 +171,52 @@ async function buttonPress(buttonIdent) {
 //  Creates the loading bar animation
 //  dynamically.
 async function loadingBarAnimation(speed) {
-//  Getting the loading bard section and replacing it
-//  with its 'active' form.
+    //  Getting the loading bard section and replacing it
+    //  with its 'active' form.
     let loadingBar = document.getElementById("load-bar");
     let tempAsset = activeLoadBar;
 
-//  The temporary asset that creates the animation
+    //  The temporary asset that creates the animation
     loadingBar.innerHTML = tempAsset;
 
-//  Replacing each unloaded character one by one
-//  wiht '\'.
-    for(i = 1; i < activeLoadBar.length - 1; i++) {
+    //  Replacing each unloaded character one by one
+    //  wiht '\'.
+    for (i = 1; i < activeLoadBar.length - 1; i++) {
         tempAsset = tempAsset.replaceAt(i, '\\');
 
-//  Adding delay.
-//  Lil' easter egg hehe, we be evil in this house.
-//  I barely had any sleep.
+        //  Adding delay.
+        //  Lil' easter egg hehe, we be evil in this house.
+        //  I barely had any sleep.
         let seconds = new Date().getTime() / 1000;
-        if(seconds % 37 == 0)
+        if (seconds % 37 == 0)
             await sleep(speed + 2000);
 
         else
             await sleep(speed);
 
-//  Updating the inner text of the loading bar for each
-//  iteration.
+        //  Updating the inner text of the loading bar for each
+        //  iteration.
         loadingBar.innerText = tempAsset;
     }
 }
 
 //  Loads every piece of text required for switching to a new page
 //  in their specific order.
-async function onInnerPageLoad()  {
-//  Getting the requried loading lines and updating 
+async function onInnerPageLoad() {
+    //  Getting the requried loading lines and updating 
     //  the one that denotes that loaded Page.
     document.getElementById("on-load-center-box").style.visibility = "hidden";
     let loadingLines = document.getElementsByClassName("loading-output");
     document.getElementById("page-identifier").innerText = fileName;
 
-//  Wating for th loading bar to finish.
+    //  Wating for th loading bar to finish.
     await loadingBarAnimation(25);
-    
-//  Instantly rendering the rest of the remaining lines.
-    for(i = 1; i < 4; i++) {
+
+    //  Instantly rendering the rest of the remaining lines.
+    for (i = 1; i < 4; i++) {
         loadingLines[i].style.visibility = "visible";
         await sleep(25);
-    }  
+    }
     document.getElementById("on-load-center-box").style.visibility = "visible";
 }
 
@@ -226,9 +226,9 @@ async function resetOnInnerPageLoad() {
     let loadingLines = document.getElementsByClassName("loading-output");
     let loadBar = document.getElementById("load-bar");
 
-//  Hiding the unused elements in reverse order and
-//  with a delay in order for a smoother transition.
-    for(i = 3; i > 0; i--) {
+    //  Hiding the unused elements in reverse order and
+    //  with a delay in order for a smoother transition.
+    for (i = 3; i > 0; i--) {
         loadingLines[i].style.visibility = "hidden";
         await sleep(25);
     }
@@ -246,8 +246,8 @@ async function loadAssets() {
 document.onload = loadAssets();
 
 //  Beahviour for buttons.
-shellButtons[0].onclick = () => {buttonPress(0)};
-shellButtons[1].onclick = () => {buttonPress(1)};
-shellButtons[2].onclick = () => {buttonPress(2)};
-shellButtons[3].onclick = () => {buttonPress(3)};
+shellButtons[0].onclick = () => { buttonPress(0) };
+shellButtons[1].onclick = () => { buttonPress(1) };
+shellButtons[2].onclick = () => { buttonPress(2) };
+shellButtons[3].onclick = () => { buttonPress(3) };
 
